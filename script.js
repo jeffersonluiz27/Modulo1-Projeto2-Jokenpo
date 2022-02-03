8853461d63703dfa24518e0a75e21eb873036b0f
jeffersonluiz27 <developer.jefferson27@outlook.com>
2022-02-03 17:11:57 -0300

    add funcionalidades

--- script.js
+++ script.js
@@ -1,24 +1,65 @@
 /**
 Projeto 02 - Jokenpô
-
-Permitir que eu decida quantas rodadas iremos fazer;
-Ler a minha escolha (Pedra, papel ou tesoura);
-Decidir de forma aleatória a decisão do computador;
-Mostrar quantas rodadas cada jogador ganhou;
-Determinar quem foi o grande campeão de acordo com a quantidade de vitórias de cada
-um (computador e jogador);
-Perguntar se o Jogador quer jogar novamente, se sim inicie volte a escolha de
-quantidade de rodadas, se não finalize o programa.
-
- */
+*/
 
 // Chamada do prompt
 const prompt = require('prompt-sync')();
 
-// Variavel que controla novo jogo
+// Variaveis principais
 let newGame = true;
+let vitoriaComputador = 0;
+let vitoriaJogador = 0;
+let jogada = ['PEDRA', 'PAPEL', 'TESOURA'];
 
-// Função para verificar a resposta
+//------------- MAIN----------------//
+// Corpo principal do programa
+do {
+  let rodadas = +prompt('Informe a quantidade de rodas: ');
+  vitoriaComputador = 0;
+  vitoriaJogador = 0;
+  let fim = 0;
+
+  for (let i = 1; i <= rodadas; i++) {
+    console.log(`Rodada ${i}\n`);
+    let computador = '';
+    let jogador = '';
+
+    //Escolha do computador
+    let escolha = Math.floor(3 * Math.random()); // Gera um numero aleatório de 0 a 2
+    computador = jogada[escolha]; //Seleciona jogada na lista usando numero gerado
+
+    //Escolha do jogador
+    jogador = escolhaJogador(); // Valida escolha
+
+    //Verifica quem ganhou a rodada
+    console.log(campeaoRodada(computador, jogador));
+    console.log(
+      `Placar: ${vitoriaComputador} Computador | ${vitoriaJogador} Jogador\n`
+    );
+
+    // Exibe o grade vencedor
+    fim++;
+    if (fim == rodadas) {
+
+      console.log('FIM');  
+    }
+    
+  }
+
+  //Pergunta se jogador quer reiniciar o jogo
+  console.log();
+  resp = respostas('Deseja jogar de novo? (1 - SIM / 2 - NÃO): ');
+  if (resp == 1) {
+    console.clear();
+    newGame = true;
+  } else {
+    newGame = false;
+  }
+} while (newGame);
+
+//------------- FUNÇÕES----------------//
+
+// Função para verificar a resposta de novo jogo
 function respostas(pergunta) {
   while (true) {
     let resposta = +prompt(pergunta);
@@ -34,20 +75,55 @@ function respostas(pergunta) {
   }
 }
 
-
-// Corpo principal do programa
-do {
-  let rodadas = +prompt('Informe a quantidade de rodas: ');
-
-  for (let i = 0; i < rodadas; i++) {
-    console.log({i});
+//Função verifica se usuario digitou as opções válidas
+function escolhaJogador() {
+  while (true) {
+    let escolha = prompt(
+      'Digite sua jogada (pedra papel ou tesoura): '
+    ).toUpperCase();
+    if (escolha === 'PEDRA' || escolha === 'PAPEL' || escolha === 'TESOURA') {
+      return escolha;
+    } else {
+      console.clear();
+      console.log('Jogada incorreta! Digite da forma correta!');
+    }
   }
+}
 
-  resp = respostas('Deseja jogar de novo? (1 - SIM / 2 - NÃO): ');
-  if (resp == 1) {
-    console.clear();
-    newGame = true;
-  } else {
-    newGame = false;
+//Função que verifica quem ganhou a rodada
+function campeaoRodada(computador, jogador) {
+  console.clear();
+  console.log(`Computador: ${computador}, Jogador: ${jogador}`);
+
+  if (computador == 'PEDRA') {
+    if (jogador == 'TESOURA') {
+      vitoriaComputador++;
+      return 'O Computador venceu essa!\n';
+    } else if (jogador == 'PAPEL') {
+      vitoriaJogador++;
+      return 'Voce venceu essa!\n';
+    } else {
+      return 'Deu empate!\n';
+    }
+  } else if (computador == 'PAPEL') {
+    if (jogador == 'PEDRA') {
+      vitoriaComputador++;
+      return 'O Computador venceu essa!\n';
+    } else if (jogador == 'TESOURA') {
+      vitoriaJogador++;
+      return 'Voce venceu essa!\n';
+    } else {
+      return 'Deu empate!\n';
+    }
+  } else if (computador == 'TESOURA') {
+    if (jogador == 'PAPEL') {
+      vitoriaComputador++;
+      return 'O Computador venceu essa!\n';
+    } else if (jogador == 'PEDRA') {
+      vitoriaJogador++;
+      return 'Voce venceu essa!\n';
+    } else {
+      return 'Deu empate!\n';
+    }
   }
-} while (newGame);
+}
